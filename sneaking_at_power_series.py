e98409d401cc4a85d573c62c5d8c7a80140d27f2
l0r3m1psum <34714625+l0r3m1psum@users.noreply.github.com>
2019-09-22 10:32:23 +0200

    Initial implementation.

---
+++ sneaking_at_power_series.py
@@ -0,0 +1,99 @@
+"""Sneaking at Power Series.
+
+This module is a Python version of Squinting at Power Series, it is also
+inspired by Power Serius another work of M. Douglas Mcllroy.
+
+Sources:
+https://swtch.com/~rsc/thread/squint.pdf
+https://www.cs.dartmouth.edu/~doug/powser.html
+https://en.wikipedia.org/wiki/Power_series"""
+
+from itertools import  tee
+
+# TODO: fare le prove con sin, cos, sinh, cosh, exp, ecc per la precisione dei numeri
+
+# F+G = f+g + F'+G'
+def add(F, G):
+	"""Adds two power series."""
+	yield next(F) + next(G)
+	yield from add(F, G)
+
+# -F = -f + -F'
+def neg(F):
+	"""Negate a power serie."""
+	yield -next(F)
+	yield from neg(F)
+
+# F-G = F + -G
+def sub(F, G):
+	"""Subtracts two power series."""
+	yield from add(F, neg(G))
+
+def mulc(const, F):
+	"""Multiply a power serie with a constant."""
+	yield const*next(F)
+	yield from mulc(const, F)
+
+# F*G = f*g + f*F' + F'*G
+def mul(F, G):
+	"""Multiplies two power series."""
+	G, G1 = tee(G)
+	f = next(F)
+	yield f * next(G)
+	yield from add(mulc(f, G), mul(F, G1))
+
+
+# Q = F/G = f/g + (1/g)*(F' - Q*G')
+def div(F, G):
+	"""Divides two power series."""
+	F, F1 = tee(F)
+	G, G1 = tee(G)
+	g = next(G)
+	yield next(F) / g
+	yield from mulc(1/g, sub(F, mul(div(F1, G1), G)))
+
+# TODO:
+# https://en.wikipedia.org/wiki/Binomial_theorem
+def exp(F, const):
+	"""Elevate a power serie to a given constant."""
+	pass
+
+# TODO:
+# F(G) = f + G'*F'(G)
+# F(G) = f + g*F' + G'*F'(G)
+# this assumes g to be 0
+def comp(F, G):
+	"""Composes two power series."""
+	G, G1 = tee(G)
+	yield next(F)
+	_ = next(G)
+	yield from mul(G, comp(F, G1))
+	# F(G) = f + F'(G)
+	# yield next(F)
+	# yield from comp(F, G)
+
+def recip(F):
+	"""Reciprocal of a power serie."""
+	pass
+
+def deriv(F):
+	"""Derivate a power serie."""
+	_ = next(F)
+	for n, a in enumerate(F, 1):
+		yield n * a
+
+def integr(F, const):
+	"""Integrate a power serie."""
+	yield const
+	for n, a in enumerate(F, 1):
+		yield 1/n * a
+
+if __name__ == '__main__':
+	# from sys import getrecursionlimit, setrecursionlimit
+	# print(getrecursionlimit()) # 1_000
+	# setrecursionlimit(10_000)
+	from itertools import islice, count, repeat
+	# lol 14 is the max, after that you hit RecursionError
+	print(list(islice(div(count(), repeat(2)), 14)))
+	# print(list(islice(div(count(), repeat(2)), 25)))
+
---
+++ test_sneaking_at_power_series.py
@@ -0,0 +1,84 @@
+import unittest
+from sneaking_at_power_series import *
+from itertools import islice, count, repeat
+
+class Tests(unittest.TestCase):
+
+	def test_add(self):
+		# elem. neutro
+		self.assertEqual(list(islice(add(count(), repeat(0)), 10)),
+		                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+		self.assertEqual(list(islice(add(repeat(0), count(0)), 10)),
+		                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+		self.assertEqual(list(islice(add(count(), count()), 10)),
+		                 [0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
+
+	def test_neg(self):
+		# elem. neutro
+		self.assertEqual(list(islice(neg(repeat(0)), 10)),
+		                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+		self.assertEqual(list(islice(neg(count(0)), 10)),
+		                 [0, -1, -2, -3, -4, -5, -6, -7, -8, -9])
+
+	def test_sub(self):
+		# elem. neutro
+		self.assertEqual(list(islice(sub(count(), repeat(0)), 10)),
+		                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+		self.assertEqual(list(islice(sub(repeat(0), count()), 10)),
+		                 [0, -1, -2, -3, -4, -5, -6, -7, -8, -9])
+		# elem. annullatore
+		self.assertEqual(list(islice(sub(count(0), count(0)), 10)),
+		                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+	def test_mul(self):
+		# elem. annullatore
+		self.assertEqual(list(islice(mul(count(), repeat(0)), 10)),
+		                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+		self.assertEqual(list(islice(mul(count(1), count(1)), 10)),
+		                 [1, 4, 10, 20, 35, 56, 84, 120, 165, 220])
+
+	def test_mulc(self):
+		# elem. annnullatore
+		self.assertEqual(list(islice(mulc(0, count()), 10)),
+		                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+		# elem neutro
+		self.assertEqual(list(islice(mulc(1, count()), 10)),
+		                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+		self.assertEqual(list(islice(mulc(-1, count()), 10)),
+		                 [0, -1, -2, -3, -4, -5, -6, -7, -8, -9])
+
+
+	def test_div(self):
+		self.assertEqual(list(islice(div(count(1), count(1)), 10)),
+		                 [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+
+	def test_deriv(self):
+		self.assertEqual(list(islice(deriv(count(0)), 10)),
+		                 [1, 4, 9, 16, 25, 36, 49, 64, 81, 100])
+		self.assertEqual(list(islice(integr(deriv(count(1)), 1), 10)),
+		                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+	def test_integr(self):
+		self.assertEqual(list(islice(integr(count(1), 0), 10)),
+		                 [0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+		self.assertEqual(list(islice(deriv(integr(count(1), 0)), 10)),
+		                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+	def test_euristics(self):
+		"""I don't know if they are correct and they should be removed."""
+		self.assertEqual(list(islice(mul(count(), repeat(1)), 10)), 
+		                 [0, 1, 3, 6, 10, 15, 21, 28, 36, 45])
+		# is it correct?
+		self.assertEqual(list(islice(comp(count(1), count(1)), 10)), 
+		                 [1, 4, 18, 76, 309, 1224, 4756, 18208, 68892, 258176])
+		 # seems legit
+		self.assertEqual(list(islice(comp(repeat(1), repeat(1)), 10)), 
+		                 [1, 1, 2, 4, 8, 16, 32, 64, 128, 256])
+
+if __name__ == '__main__':
+	unittest.main()
